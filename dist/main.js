1f3ae1e1711fe464b4ab73d68ccd477036036b75
Karim Khoja <karimkhoja13@yahoo.com>
2023-11-07 17:59:45 -0800

    sort the Pigs array

--- dist/main.js
+++ dist/main.js
@@ -1,30 +1,125 @@
 "use strict";
-class GreyPig {
-    constructor(name, breed, height, weight, personality, swimmingAbility) {
-        this.name = name;
-        this.breed = breed;
-        this.height = height;
-        this.weight = weight;
-        this.personality = personality;
-        this.swimmingAbility = swimmingAbility;
-    }
-    displayInfo() {
-        return `Name: ${this.name}\nBreed: ${this.breed}\nHeight: ${this.height}\nWeight: ${this.weight}\nPersonality: ${this.personality}\nSwimming Ability: ${this.swimmingAbility}`;
-    }
-}
-var pigs = [];
-pigs.push(new GreyPig('Pork Chop', 'White', 15, 30, 'Brave', 80));
-console.log(pigs);
-let addButton = document.getElementById('add-button'); // get the reference to the add button
-let addForm = document.getElementById('add-form'); // get the reference to the add form
-addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener('click', () => {
-    // add a click event listener to the add button
-    if ((addForm === null || addForm === void 0 ? void 0 : addForm.style.display) === 'block') {
-        // if the add form is visible
-        addForm.style.display = 'none'; // hide the add form
-    }
-    else if ((addForm === null || addForm === void 0 ? void 0 : addForm.style.display) === 'none') {
-        // if the add form is hidden
-        addForm.style.display = 'block'; // show the add form
-    }
-});
+// interface IPig {
+//   name: string;
+//   breed: string;
+//   height: number;
+//   weight: number;
+//   personality: string;
+//   displayInfo(): string;
+// }
+// class pig implements IPig {
+//   name: string;
+//   breed: string;
+//   height: number;
+//   weight: number;
+//   personality: string;
+//   constructor(
+//     name: string,
+//     breed: string,
+//     height: number,
+//     weight: number,
+//     personality: string
+//   ) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.height = height;
+//     this.weight = weight;
+//     this.personality = personality;
+//   }
+//   public displayInfo(): string {
+//     return `<tr><td>Name: ${this.name}\n
+//             Breed: ${this.breed}\n
+//             Height: ${this.height}\n
+//             Weight: ${this.weight}\n
+//             Personality: ${this.personality}
+//             </td></tr>`;
+//   }
+// }
+// class GreyPig extends pig {
+//   swimmingAbility: number; // unique attribute for Grey pigs
+//   constructor(
+//     name: string,
+//     breed: string,
+//     height: number,
+//     weight: number,
+//     personality: string,
+//     swimmingAbility: number
+//   ) {
+//     super(name, breed, height, weight, personality); // call the super class constructor
+//     this.swimmingAbility = swimmingAbility;
+//   }
+//   displayInfo(): string {
+//     return super.displayInfo() + `\nSwimming Ability: ${this.swimmingAbility}`;
+//   }
+// }
+// function displayAllPigs(pig: IPig): string {
+//   // let info = `Name: ${pig.name}\nBreed: ${pig.breed}\nHeight: ${pig.height}\nWeight: ${pig.weight}\nPersonality: ${pig.personality}\n`;
+//   let info = `<tr><td>Name: ${pig.name}</td><td>Category: ${pig.constructor.name}</td><td><a>More Info</a></td><td><a>Delete</a></td>: ${pig.height}\nWeight: ${pig.weight}\nPersonality: ${pig.personality}\n`;
+//   switch (pig.constructor.name) {
+//     case 'GreyPig':
+//       info += `Swimming Ability: ${(pig as GreyPig).swimmingAbility}`;
+//       break;
+//     // case 'ChestnutPig':
+//     //   info += `Language: ${(pig as ChestnutPig).language}`;
+//     //   break;
+//     // case 'WhitePig':
+//     //   info += `Running Ability: ${(pig as WhitePig).runningAbility}`;
+//     //   break;
+//     // case 'BlackPig':
+//     //   info += `Strength: ${(pig as BlackPig).strength}`;
+//     //   break;
+//   }
+//   return info;
+// }
+// var pigs: IPig[] = [];
+// pigs.push(new GreyPig('Pork Chop', 'Black', 15, 30, 'Brave', 80));
+// console.log(pigs);
+// let pigTable = document.getElementById('pig-table') as HTMLTableElement;
+// function showAllPigs() {
+//   pigTable.querySelector('tbody')!.innerHTML = ' ';
+//   pigs.forEach((pig, index) => {
+//     // loop through the pigs array
+//     let category = pig.constructor.name; // get the category from the constructor name
+//     let row = document.createElement('tr'); // create a new table row
+//     row.innerHTML = `<td>${pig.name}</td><td>${category}</td>`; // append the name and category
+//     let actionCell = document.createElement('td'); // create a cell for the actions
+//     let moreInfoButton = document.createElement('button'); // create a button for more info
+//     moreInfoButton.className = 'btn btn-info';
+//     moreInfoButton.textContent = 'More info';
+//     moreInfoButton.addEventListener('click', () => {
+//       // add a click event listener
+//       alert(pig.displayInfo()); // display the pig's information using the displayInfo method
+//     });
+//     actionCell.appendChild(moreInfoButton); // append the button to the cell
+//     let deleteButton = document.createElement('button'); // create a button for delete
+//     deleteButton.className = 'btn btn-danger';
+//     deleteButton.textContent = 'Delete';
+//     deleteButton.addEventListener('click', () => {
+//       // add a click event listener
+//       if (confirm(`Are you sure you want to delete ${pig.name}?`)) {
+//         // ask for confirmation
+//         pigs.splice(index, 1); // remove the pig from the array
+//         localStorage.setItem('pigs', JSON.stringify(pigs)); // update the local storage
+//         showAllPigs(); // update the table
+//       }
+//     });
+//     actionCell.appendChild(deleteButton); // append the button to the cell
+//     row.appendChild(actionCell); // append the cell to the row
+//     pigTable.querySelector('tbody')!.appendChild(row); // append the row to the table body
+//   });
+// }
+// showAllPigs();
+// // const elem: any = document?.getElementById('add-form');
+// // elem.innerHTML = pigs[0].displayInfo();
+// let addButton = document.getElementById('add-button'); // get the reference to the add button
+// let addForm = document.getElementById('add-form'); // get the reference to the add form
+// addButton?.addEventListener('click', () => {
+//   // add a click event listener to the add button
+//   if (addForm?.style.display === 'block') {
+//     // if the add form is visible
+//     addForm.style.display = 'none'; // hide the add form
+//   } else if (addForm?.style.display === 'none') {
+//     // if the add form is hidden
+//     addForm.style.display = 'block'; // show the add form
+//   }
+// });
---
+++ dist/test.js
@@ -0,0 +1,214 @@
+"use strict";
+class Pig {
+    constructor(name, breed, height, weight, personality, category) {
+        this.name = name;
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+        this.personality = personality;
+        this.category = category;
+    }
+    displayInfo() {
+        return ` `;
+    }
+}
+class GreyPig extends Pig {
+    constructor(name, breed, height, weight, personality, category, swimmingAbility) {
+        super(name, breed, height, weight, personality, category); // call the super class constructor
+        this.swimmingAbility = swimmingAbility;
+    }
+    displayInfo() {
+        return super.displayInfo() + `testing super: `;
+    }
+}
+class ChestnutPig extends Pig {
+    constructor(name, breed, height, weight, personality, category, language) {
+        super(name, breed, height, weight, personality, category); // call the super class constructor
+        this.language = language;
+    }
+    displayInfo() {
+        return super.displayInfo() +
+            `testing chestnut super: `;
+    }
+}
+//   class WhitePig extends Pig {
+//     runningAbility: number; // unique attribute for Grey pigs
+//     constructor(
+//       name: string,
+//       breed: string,
+//       height: number,
+//       weight: number,
+//       personality: string,
+//       category: string,
+//       runningAbility: number
+//     ) {
+//       super(name, breed, height, weight, personality, category); // call the super class constructor
+//       this.runningAbility = runningAbility;
+//     }
+//     public displayInfo(): string {
+//       return super.displayInfo() + `testing whitepig super: `;
+//     }
+//   }
+//   class BlackPig extends Pig {
+//     strengthAbility: number; // unique attribute for Grey pigs
+//     constructor(
+//       name: string,
+//       breed: string,
+//       height: number,
+//       weight: number,
+//       personality: string,
+//       category: string,
+//       strengthAbility: number
+//     ) {
+//       super(name, breed, height, weight, personality, category); // call the super class constructor
+//       this.strengthAbility = strengthAbility;
+//     }
+//     public displayInfo(): string {
+//       return super.displayInfo() +
+//         `blackpig super: `;
+//     }
+//   }
+function displayPig(pig) {
+    return `<Name: ${pig.name}`;
+    +`Name: ${pig.name}` +
+        `Name: ${pig.name}` +
+        `Name: ${pig.name}` +
+        `Name: ${pig.name}` +
+        `Name: ${pig.name}`;
+}
+// Get the references to the HTML elements
+let addButton = document.getElementById('add-button');
+let pigTable = document.getElementById('pig-table');
+let moreInfoTable = document.getElementById('more-info');
+let pigDetailsSection = document.getElementById('pig_details');
+let addForm = document.getElementById('add-form');
+let categorySelect = document.getElementById('category-select');
+let nameInput = document.getElementById('name-input');
+let breedInput = document.getElementById('breed-input');
+let heightInput = document.getElementById('height-input');
+let weightInput = document.getElementById('weight-input');
+let personalityInput = document.getElementById('personality-input');
+let uniqueAttribute = document.getElementById('unique-attribute');
+let submitButton = document.getElementById('submit-button');
+let cancelButton = document.getElementById('cancel-button');
+// Load the pigs from the local storage
+let pigs = JSON.parse(localStorage.getItem('pigs') || '[]');
+pigs.sort((a, b) => a.category.localeCompare(b.category));
+console.log(pigs[0]);
+//console.log(pigs[0]);
+// Populate the table with the pigs
+function populateTable() {
+    pigTable.querySelector('tbody').innerHTML = ''; // clear the table
+    pigs.forEach((pig, index) => {
+        // loop through the pigs array
+        // let category = pig.constructor.name; // get the category from the constructor name
+        let row = document.createElement('tr'); // create a new table row
+        row.innerHTML = `<td>${pig.name}</td><td>${pig.category}</td>`; // append the name and category
+        let actionCell = document.createElement('td'); // create a cell for the actions
+        let moreInfoButton = document.createElement('button'); // create a button for more info
+        moreInfoButton.className = 'btn btn-info';
+        moreInfoButton.textContent = 'More info';
+        console.log(pig);
+        moreInfoButton.addEventListener('click', () => {
+            // add a click event listener
+            moreInfoTable.querySelector('tbody').innerHTML = '';
+            let t_row = document.createElement('tr');
+            t_row.innerHTML = `<td>${pig.name}</td><td>${pig.category}</td>`;
+            // alert(displayPig(pig)); // display the pig's information using the displayInfo method
+            moreInfoTable.querySelector('tbody').appendChild(t_row); // append the row to the table body
+            addForm.style.display = 'none'; // hide the form
+            addButton.style.display = 'block'; // show the button
+            pigDetailsSection.style.display = 'block'; // show the button
+            populateTable();
+        });
+        actionCell.appendChild(moreInfoButton); // append the button to the cell
+        let deleteButton = document.createElement('button'); // create a button for delete
+        deleteButton.className = 'btn btn-danger';
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            // add a click event listener
+            if (confirm(`Are you sure you want to delete ${pig.name}?`)) {
+                // ask for confirmation
+                pigs.splice(index, 1); // remove the pig from the array
+                localStorage.setItem('pigs', JSON.stringify(pigs)); // update the local storage
+                populateTable(); // update the table
+            }
+        });
+        actionCell.appendChild(deleteButton); // append the button to the cell
+        row.appendChild(actionCell); // append the cell to the row
+        pigTable.querySelector('tbody').appendChild(row); // append the row to the table body
+    });
+}
+populateTable(); // call the function to populate the table initially
+// Show the add form when the add button is clicked
+addButton.addEventListener('click', () => {
+    addForm.style.display = 'block'; // show the form
+    addButton.style.display = 'none'; // hide the button
+    pigDetailsSection.style.display = 'none'; // hide the button
+});
+// Hide the add form when the cancel button is clicked
+cancelButton.addEventListener('click', () => {
+    addForm.style.display = 'none'; // hide the form
+    addButton.style.display = 'block'; // show the button
+});
+// Populate the unique attribute based on the selected category
+function populateUniqueAttribute() {
+    uniqueAttribute.innerHTML = ''; // clear the unique attribute
+    let category = categorySelect.value; // get the selected category
+    if (category === 'Grey') {
+        // if the category is Grey
+        uniqueAttribute.innerHTML =
+            "<label for='swimming-input'>Enter a swimming ability:</label><input id='swimming-input' type='number' class='form-control'>"; // append a label and an input for swimming ability
+    }
+    else if (category === 'Chestnut') {
+        // if the category is Chestnut
+        uniqueAttribute.innerHTML =
+            "<label for='language-input'>Enter a language:</label><input id='language-input' type='text' class='form-control'>"; // append a label and an input for language
+    }
+    else if (category === 'White') {
+        // if the category is White
+        uniqueAttribute.innerHTML =
+            "<label for='running-input'>Enter a running ability:</label><input id='running-input' type='number' class='form-control'>"; // append a label and an input for running ability
+    }
+    else if (category === 'Black') {
+        // if the category is Black
+        uniqueAttribute.innerHTML =
+            "<label for='strength-input'>Enter a strength:</label><input id='strength-input' type='number' class='form-control'>"; // append a label and an input for strength
+    }
+}
+populateUniqueAttribute(); // call the function to populate the unique attribute initially
+// Update the unique attribute when the category is changed
+categorySelect.addEventListener('change', () => {
+    populateUniqueAttribute();
+});
+// Add a new pig when the submit button is clicked
+submitButton.addEventListener('click', () => {
+    let category = categorySelect.value; // get the selected category
+    let name = nameInput.value; // get the name
+    let breed = breedInput.value; // get the breed
+    let height = heightInput.valueAsNumber; // get the height
+    let weight = weightInput.valueAsNumber; // get the weight
+    let personality = personalityInput.value;
+    let newPig;
+    if (category === 'Grey') {
+        let swimmingAbility = document.getElementById('swimming-input').valueAsNumber;
+        newPig = new GreyPig(name, breed, height, weight, personality, "Grey Pig", swimmingAbility);
+    }
+    else if (category === 'Chestnut') {
+        let language = document.getElementById('language-input').value;
+        newPig = new ChestnutPig(name, breed, height, weight, personality, "Chestnut Pig", language);
+    }
+    else if (category === 'White') {
+        let runningAbility = document.getElementById('running-input').valueAsNumber;
+        newPig = new WhitePig(name, breed, height, weight, personality, "White Pig", runningAbility);
+    }
+    else if (category === 'Black') {
+        let strength = document.getElementById('strength-input').valueAsNumber;
+        newPig = new BlackPig(name, breed, height, weight, personality, "Black Pig", strength);
+    }
+    pigs.push(newPig); // add the new pig to the array
+    localStorage.setItem('pigs', JSON.stringify(pigs)); // update the local storage
+    populateTable(); // update the table
+    addForm.style.display = 'none'; // hide the form
+    addButton.style.display = 'block'; // show the button
+});
